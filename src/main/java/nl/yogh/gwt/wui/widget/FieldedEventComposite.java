6a384b540bcc4fcbc253732e789f792ad65243d6
Jorn C <jorncruijsen@gmail.com>
2017-12-07 09:44:42 +0100

    Bus in UiBinder

---
+++ src/main/java/nl/yogh/gwt/wui/widget/FieldedEventComposite.java
@@ -0,0 +1,28 @@
+package nl.yogh.gwt.wui.widget;
+
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.web.bindery.event.shared.EventBus;
+
+public abstract class FieldedEventComposite extends Composite implements HasEventBus {
+  public @UiField(provided = true) EventBus eventBus;
+
+  public FieldedEventComposite() {}
+
+  public FieldedEventComposite(final EventBus eventBus) {
+    setEventBus(eventBus);
+  }
+
+  @Override
+  public void setEventBus(final EventBus eventBus) {
+    this.eventBus = eventBus;
+  }
+
+  protected void setEventBus(final EventBus eventBus, final HasEventBus... composites) {
+    this.eventBus = eventBus;
+
+    for (final HasEventBus comp : composites) {
+      comp.setEventBus(eventBus);
+    }
+  }
+}
