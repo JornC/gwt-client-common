3a20c1f759e3392fd3a62557e41053cb6efdea6a
Jorn C <jorncruijsen@gmail.com>
2017-12-07 09:45:01 +0100

    NotificationPanel in Widgets

---
+++ src/main/java/nl/yogh/gwt/wui/widget/NotificationPanel.java
@@ -0,0 +1,25 @@
+package nl.yogh.gwt.wui.widget;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.web.bindery.event.shared.EventBus;
+import com.google.web.bindery.event.shared.binder.EventBinder;
+import com.google.web.bindery.event.shared.binder.EventHandler;
+
+import nl.yogh.gwt.wui.event.NotificationEvent;
+
+public class NotificationPanel extends FlowPanel implements HasEventBus {
+  private final NotificationPanelEventBinder EVENT_BINDER = GWT.create(NotificationPanelEventBinder.class);
+
+  interface NotificationPanelEventBinder extends EventBinder<NotificationPanel> {}
+
+  @Override
+  public void setEventBus(final EventBus eventBus) {
+    EVENT_BINDER.bindEventHandlers(this, eventBus);
+  }
+
+  @EventHandler
+  public void onNotification(final NotificationEvent event) {
+    add(new NotificationWidget(event.getValue()));
+  }
+}
