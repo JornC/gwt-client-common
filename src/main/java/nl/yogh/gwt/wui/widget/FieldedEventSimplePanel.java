710e9afed8052c3eaa43e6c4b3f8d2c872dc47cb
Jorn Cruijsen <jorncruijsen@gmail.com>
2017-12-12 19:07:07 +0100

    More fielded widget types

---
+++ src/main/java/nl/yogh/gwt/wui/widget/FieldedEventSimplePanel.java
@@ -0,0 +1,26 @@
+package nl.yogh.gwt.wui.widget;
+
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.web.bindery.event.shared.EventBus;
+
+public class FieldedEventSimplePanel extends SimplePanel implements HasEventBus {
+  public @UiField(provided = true) EventBus eventBus;
+
+  public FieldedEventSimplePanel(final EventBus eventBus) {
+    setEventBus(eventBus);
+  }
+
+  @Override
+  public void setEventBus(final EventBus eventBus) {
+    this.eventBus = eventBus;
+  }
+
+  protected void setEventBus(final EventBus eventBus, final HasEventBus... composites) {
+    this.eventBus = eventBus;
+
+    for (final HasEventBus comp : composites) {
+      comp.setEventBus(eventBus);
+    }
+  }
+}
